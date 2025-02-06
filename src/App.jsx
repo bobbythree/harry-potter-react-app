8e1c741b5b52a30a0a920e8d1b04ebbda477d52b
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-06 16:48:52 -0600

    add font and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import BookList from "./components/BookList"
 import SpellList from "./components/SpellList"
 import CharacterList from "./components/CharacterList"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <>
+      <Header />
       <BookList />
       <CharacterList />
       <SpellList />
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <h1 className='heading text-secondary mt-40 mb-10'>Harry Potter</h1>
+  )
+}
