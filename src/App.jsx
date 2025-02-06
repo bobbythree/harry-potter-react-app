2251067af491b00850b23c47cf69211b3a0f7934
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-06 15:28:10 -0600

    characters list collapse

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import BookList from "./components/BookList"
 import SpellList from "./components/SpellList"
+import CharacterList from "./components/CharacterList"
 
 export default function App() {
   return (
     <>
       <BookList />
+      <CharacterList />
       <SpellList />
     </>
   )
