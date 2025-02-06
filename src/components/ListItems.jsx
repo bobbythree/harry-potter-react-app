bf0267694f7649435a94f0731c84de7b3540ce0d
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-06 14:23:08 -0600

    book titles collapse

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -13,19 +13,26 @@ export default function ListItems() {
   }
 
   return (
-    <div className="flex flex-col items-center">    
-      {data && data.map((book, index) => (
-        <List key={index}>
-          <li className="list-row">
-            <div><img className="size-12 rounded-sm" src={book.cover}/></div>
-            <div>
-              <div className="text-primary-content">{`Book ${book.number}`}</div>
-              <div className="text-secondary text-lg">{book.title}</div>
-            </div>
-            <p className="list-col-wrap text-xs">{book.description}</p>
-            </li>
-        </List>
-      ))}      
+    <div className="flex flex-col items-center content-center text-center">
+      <div class="collapse collapse-arrow bg-base-200 border border-base-300 w-1/2">
+        <input type="checkbox" />
+        <div class="collapse-title font-semibold">Book Titles</div>
+        <div class="collapse-content text-sm flex flex-col items-center">
+          {data && data.map((book, index) => (
+            <List key={index}>
+              <li className="list-row">
+                <div><img className="size-12 rounded-sm" src={book.cover}/></div>
+                <div>
+                  <div className="text-primary-content">{`Book ${book.number}`}</div>
+                  <div className="text-secondary text-lg">{book.title}</div>
+                </div>
+                <p className="list-col-wrap text-xs">{book.description}</p>
+                </li>
+            </List>
+          ))}      
+        </div>
+      </div>
+    
     </div>
   );
 }
