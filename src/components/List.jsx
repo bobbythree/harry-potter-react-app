92e7c82f5acf519fd97684179bcd50585be4865c
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-10 20:47:26 -0600

    code cleanup

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,8 +2,8 @@ export default function List({children, title}) {
   return (
     <div className="grid place-items-center">
       <ul className="list rounded-sm shadow-md m-1 md:w-1/2 w-7/8">  
-        <div className="flex flex-col items-center content-center font-primary">
-          <div className="collapse collapse-arrow bg-base-100  text-center grid place-items-center">
+        <div className="font-primary">
+          <div className="collapse collapse-arrow bg-base-100 text-center grid place-items-center">
             <input type="checkbox" />
             <div className="collapse-title font-semibold">{title}</div>
             <div className="collapse-content text-sm flex flex-col items-center text-start w-7/8 p-1 gap-2">
