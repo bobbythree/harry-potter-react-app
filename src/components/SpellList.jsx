fe873e52d8eced76727fd40f3fd3576e63a95087
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-10 19:46:07 -0600

    spells refactor complete

--- src/components/SpellList.jsx
+++ src/components/SpellList.jsx
@@ -18,26 +18,25 @@ export default function SpellList() {
     }
 
   return (
-    <div className="flex flex-col items-center content-center text-center font-primary">
-      <div className="collapse collapse-arrow bg-base-100 border border-base-300 md:w-1/2 w-7/8">
-        <input type="checkbox" />
-        <div className="collapse-title font-semibold">Spells</div>
-        <div className="collapse-content text-sm grid lg:grid-cols-2 grid-cols-1 text-start place-items-center">
-          {data && data.map((spell, index) => (
-            <List key={index}>
-              <li className="list-row h-auto bg-base-300">
-                <div>
-                  <div className="text-secondary text-lg">{spell.spell}</div>
-                </div>
-                <p className="list-col-wrap text-xs">{spell.use}</p>
-                </li>
-            </List>
-          ))}      
-        </div>
+    <>
+    <List title="Spells">
+      <div className="grid sm:grid-cols-2  grid-cols-1 gap-2 h-auto">
+      {data && data.map((spell, index) => (
+          <div key={index} className="bg-warning-content p-1 w-2/2">
+          <li className="list-row h-2/2 bg-base-300">
+            <div>
+              <div className="text-secondary text-lg">{spell.spell}</div>
+            </div>
+            <p className="list-col-wrap text-xs">{spell.use}</p>
+          </li>
+          </div>
+        
+        
+      ))}      
       </div>
-    
-    </div>
-  );
+    </List>
+    </>
+  )   
 }
 
 //funcs
