f0d1f998c5d1c279670a708570d54f2e67418d74
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-16 10:26:05 -0600

    heading styles refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
-    <h1 className='heading mt-40 mb-10'>
-      <span className="text-transparent bg-clip-text bg-gradient-to-b from-secondary to-secondary-content">Harry Potter</span>
+    <h1 className='heading mt-50 mb-15 text-center'>
+      <span className=
+      "font-[HarryP] md:text-9xl text-8xl text-transparent bg-clip-text bg-gradient-to-b from-secondary to-secondary-content">
+        Harry Potter
+      </span>
     </h1>
   )
 }
